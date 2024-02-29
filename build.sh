1ff995eb339fa73e587aaca6195ce6766d94c3f3
TelmoLewis <124844690+TelmoLewis@users.noreply.github.com>
2024-02-29 16:15:04 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ docker build -t flask-image .
 docker build -t nginx-image -f Dockerfile.nginx .
 
 docker run -d --network jenkins-network --name flask-app flask-image
-docker run -d -p 80:80 --name webserver --network jenkins-network nginx:latest
+docker run -d -p 80:80 --name webserver --network jenkins-network nginx-image
